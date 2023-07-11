8953f92f2be9199f9d1e73a2b414310fde41a31c
FrankM-7 <frankm-7@tamu.edu>
2023-07-11 14:24:51 -0500

    heroku builds

--- app.py
+++ app.py
@@ -6,16 +6,30 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from firebase_admin import firestore
-#from flask_cors import CORS #comment this on deployment
-
-# Fetch the service account key JSON file contents
-cred = credentials.Certificate('credentials.json')
-fb_admin = firebase_admin.initialize_app(cred)
-db = firestore.client()
-
 import pyrebase
 from dotenv import load_dotenv
 import os
+
+#from flask_cors import CORS #comment this on deployment
+
+# Fetch the service account key JSON file contents
+credConfig = {
+    "type": os.getenv("type"),
+    "project_id": os.getenv("project_id"),
+    "private_key_id": os.getenv("private_key_id"),
+    "private_key": os.getenv("private_key").replace('\\n', '\n'),
+    "client_email": os.getenv("client_email"),
+    "client_id": os.getenv("client_id"),
+    "auth_uri": os.getenv("auth_uri"),
+    "token_uri": os.getenv("token_uri"),
+    "auth_provider_x509_cert_url": os.getenv("auth_provider_x509_cert_url"),
+    "client_x509_cert_url": os.getenv("client_x509_cert_url"),
+    "universe_domain": os.getenv("universe_domain"),
+}
+  
+cred = credentials.Certificate(credConfig)
+fb_admin = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 load_dotenv()
 

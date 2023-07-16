19ba1d03beee864d3689bd52b233a1d461fc869b
Frank Martinez <frankm-7@tamu.edu>
2023-07-16 17:34:32 -0500

    updated: login page to add backedn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import LoginForm from './Authentication/Login';
-import RegisterForm from './Authentication/Register';
 import Homepage from './Homepage/Homepage';
 import LoginPage from './LoginPage/LoginPage';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 
 function App() {
   const [token, setToken] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     // Check if user is logged in and set token
     const storedToken = localStorage.getItem('token');
     if (storedToken) {
       setToken(storedToken);
+      navigate('/home'); // Redirect to homepage after token is found
+    } else {
+      navigate('/'); // Redirect to login page if no token is found
     }
-  }, []);
-
+  }, [navigate]);
 
   function handleLogin(token) {
     // Perform login logic
     setToken(token);
     localStorage.setItem('token', token);
+    navigate('/home'); // Redirect to homepage after successful login
   }
 
   function handleRegister() {
@@ -32,24 +35,14 @@ function App() {
     setToken(null);
     localStorage.removeItem('token');
     console.log('Logged out');
+    navigate('/'); // Redirect to login page after logout
   }
 
   return (
-    /* <div className="App">
-        {!token ? (
-        <div>
-          <h2>Login</h2>
-          <LoginForm onLogin={handleLogin} />
-          <h2>Register</h2>
-          <RegisterForm onRegister={handleRegister} />
-        </div>
-      ) : (
-          <Homepage onLogout={handleLogout} />
-      )} 
-    </div> */
-    <div className="App">
-      <LoginPage/>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<LoginPage onLogin={handleLogin} />} />
+      <Route path="/home" element={<Homepage onLogout={ handleLogout } />} />
+    </Routes>
   );
 }
 

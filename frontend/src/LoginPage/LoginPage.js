f08173f532cd9e0ee4cfb14bb4aa7ec145d5574c
Brandon <brandon.delgado@email.com>
2023-07-17 23:54:20 -0500

    updated login page

--- frontend/src/LoginPage/LoginPage.js
+++ frontend/src/LoginPage/LoginPage.js
@@ -5,38 +5,8 @@ import TextField from '@mui/material/TextField';
 import './LoginPage.css';
 import Switch from '@mui/material/Switch';
 import google from './google.png'
-import axios from 'axios';
-import { useState } from 'react';
 
-export default function BasicGrid( { onLogin } ) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault(); // Prevent the default form submission behavior
-
-    // Create an object with the email and password data
-    const data = {
-      email: email,
-      password: password
-    };
-
-    // Make a POST request to your backend endpoint using Axios
-    axios.post('/login', data)
-      .then(response => {
-        // Handle the response from the backend if needed
-        console.log(response.data);
-        // set the token in localStorage
-        localStorage.setItem('token', response.data.token);
-        // reload window
-
-        onLogin(response.data.token);
-      })
-      .catch(error => {
-        // Handle any errors that occurred during the request
-        console.error(error);
-      });
-  };
+export default function BasicGrid() {
   return (
     <Grid className='loginBackground' sx={{height: window.innerHeight}}>
       <Grid className='welcomeBackground'>
@@ -66,35 +36,6 @@ export default function BasicGrid( { onLogin } ) {
           </Button>
           <p></p>
         </Grid>
-        {/*
-        <Grid className='loginSlideBar'>
-          <Button className='loginSlideLogin' variant="contained" disableElevation>
-            Login
-          </Button>
-          <Button className='loginSlideRegister' variant="contained" disableElevation>
-            Register
-          </Button>
-        </Grid>
-        <form onSubmit={handleSubmit}>
-          <Grid className='credentialTexts'>
-              <p className='credentialFont'>
-                email
-              </p>
-              <TextField id="outlined-basic" label="Email address" variant="outlined" className='credentialBoxes' value={email} onChange={(e) => setEmail(e.target.value)} />
-              <p className='credentialFont'>
-                password
-              </p>
-              <TextField id="outlined-basic" label="Password" variant="outlined" className='credentialBoxes' value={password} onChange={(e) => setPassword(e.target.value)} />
-          </Grid>
-          <Grid className='signInArea'>
-            <p></p>
-            <Button variant="contained" disableElevation className='signInButton' type='submit'>
-              Sign In
-            </Button>
-            <p></p>
-          </Grid>
-        </form>
-        */}
         <Grid className='continueArea'>
           <div className="line"></div>
           <p className='continueWith'>Or</p>
